505ee033ceff6f7e47cf15e2089731b40f4fb405
pe <pedroscarselletta@gmail.com>
2020-08-23 02:17:15 +0200

    comment

--- app/Http/Controllers/Api/VentaController.php
+++ app/Http/Controllers/Api/VentaController.php
@@ -72,61 +72,61 @@ class VentaController extends Controller
 
     public function importOrderFromTiendaNube(Request $request)
     {
-        echo 'llego algo para crear';
+        // echo 'llego algo para crear';
         \Log::error('llego algo para crear');
-        $hmac_header = $request->header('HTTP_X_LINKEDSTORE_HMAC_SHA256');;
-        $data = file_get_contents('php://input');
-        // // dd($data);
-        if ( $hmac_header == hash_hmac('sha256', $data, env('yllh6Sr1u0TSzYOQ6zyr1bPS2hQ42nmSPsOfomL2BPDdNy4x', 'falta')) )
-        {
-            \Log::error('create order validado ok ok');
-            // Obtener id de la venta
-            $order_id = 279936732;
-            $venta = Venta::importOrderFromTiendaNubeById($order_id);
+        // $hmac_header = $request->header('HTTP_X_LINKEDSTORE_HMAC_SHA256');;
+        // $data = file_get_contents('php://input');
+        // // // dd($data);
+        // if ( $hmac_header == hash_hmac('sha256', $data, env('yllh6Sr1u0TSzYOQ6zyr1bPS2hQ42nmSPsOfomL2BPDdNy4x', 'falta')) )
+        // {
+        //     \Log::error('create order validado ok ok');
+        //     // Obtener id de la venta
+        //     $order_id = 279936732;
+        //     $venta = Venta::importOrderFromTiendaNubeById($order_id);
 
-            echo $venta->pagada.'|';
-            echo $venta->tieneGiftcards();
+        //     echo $venta->pagada.'|';
+        //     echo $venta->tieneGiftcards();
 
-            if ( $venta->pagada && $venta->tieneGiftcards() )
-            {
-                echo 'si';
-                $venta->notify(new GiftCardMailNotification);
-            }
-        }
-        else
-        {
-            echo 'mensjage no validado';
-            \Log::error('Mensaje no validado: ' . json_encode($data));
-        }
+        //     if ( $venta->pagada && $venta->tieneGiftcards() )
+        //     {
+        //         echo 'si';
+        //         $venta->notify(new GiftCardMailNotification);
+        //     }
+        // }
+        // else
+        // {
+        //     echo 'mensjage no validado';
+        //     \Log::error('Mensaje no validado: ' . json_encode($data));
+        // }
     }
 
     public function updateOrderFromTiendaNube(Request $request)
     {
         \Log::error('llego algo para update');
-        $hmac_header = $request->header('HTTP_X_LINKEDSTORE_HMAC_SHA256');;
-        $data = file_get_contents('php://input');
+        // $hmac_header = $request->header('HTTP_X_LINKEDSTORE_HMAC_SHA256');;
+        // $data = file_get_contents('php://input');
 
-        if ( $hmac_header == hash_hmac('sha256', $data, env('yllh6Sr1u0TSzYOQ6zyr1bPS2hQ42nmSPsOfomL2BPDdNy4x', 'falta')) )
-        {
-            \Log::error('mensajke de update wvaldiado');
-            \Log::error('data: ' . $data);
-            \Log::error('data: ' . json_decode($data, true)['id']);
-            // Obtener id de la venta
-            // $order_id = 279936732;
-            // $venta = Venta::importOrderFromTiendaNubeById($order_id);
+        // if ( $hmac_header == hash_hmac('sha256', $data, env('yllh6Sr1u0TSzYOQ6zyr1bPS2hQ42nmSPsOfomL2BPDdNy4x', 'falta')) )
+        // {
+        //     \Log::error('mensajke de update wvaldiado');
+        //     \Log::error('data: ' . $data);
+        //     \Log::error('data: ' . json_decode($data, true)['id']);
+        //     // Obtener id de la venta
+        //     // $order_id = 279936732;
+        //     // $venta = Venta::importOrderFromTiendaNubeById($order_id);
 
-            // echo $venta->pagada.'|';
-            // echo $venta->tieneGiftcards();
+        //     // echo $venta->pagada.'|';
+        //     // echo $venta->tieneGiftcards();
 
-            // if ( $venta->pagada && $venta->tieneGiftcards() )
-            // {
-            //     echo 'si';
-            //     $venta->notify(new GiftCardMailNotification);
-            // }
-        }
-        else
-        {
-            \Log::error('Mensaje update no validado');
-        }
+        //     // if ( $venta->pagada && $venta->tieneGiftcards() )
+        //     // {
+        //     //     echo 'si';
+        //     //     $venta->notify(new GiftCardMailNotification);
+        //     // }
+        // }
+        // else
+        // {
+        //     \Log::error('Mensaje update no validado');
+        // }
     }
 }
